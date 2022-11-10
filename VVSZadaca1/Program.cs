87fe64b5d2ad7bfc763b89e2762074dbde6d0bb1
muhamed-droid <mmasnopita1@etf.unsa.ba>
2022-11-10 15:59:08 +0100

    Add project files.

---
+++ VVSZadaca1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VVSZadaca1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
