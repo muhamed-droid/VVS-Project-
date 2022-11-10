0b82e33709fd5ec3dcea0bfabe3280e36b6f6275
selma <selmakurtovic@live.com>
2022-11-10 16:13:18 +0100

    inital commit

--- VVSZadaca1/Program.cs
+++ VVSZadaca1/Program.cs
@@ -6,7 +6,7 @@ namespace VVSZadaca1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Selma <3!");
         }
     }
 }
