151665fe9bf3d99ab6f185a2b91a9f4909338e33
muhamed-droid <mmasnopita1@etf.unsa.ba>
2022-12-11 18:16:59 +0100

    Sad prolaze svi baš, još dd testove treba napraviti

--- VVSZadaceTest/Funkcionalnost3Test.cs
+++ VVSZadaceTest/Funkcionalnost3Test.cs
@@ -12,111 +12,111 @@ namespace VVSZadaceTests
     [TestClass()]
     public class Funkcionalnost3Test
     {
-        static Registar r;
-        static Glasac g1, g2;
-        static Kandidat k1, k2;
+        static Registar registar;
+        static Glasac glasac1, glasac2;
+        static Kandidat kandidat1, kandidat2;
         static Stranka s1, s2;
 
         [ClassInitialize()]
         public static void Initialize(TestContext context)
         {
-            r = new Registar();
+            registar = new Registar();
             s1 = new Stranka("SDP", "Socijaldemokratska partija");
             s2 = new Stranka("SPK", "Samo pošteno komunistički");
-            r.dodajGlasaca(new Glasac("Muhamed", "Masnopita", new Adresa("Iljas", "Ljesevo", 71380, "252"), new DateTime(1992, 11, 15), "165T214", "1211992111111"));
-            r.dodajGlasaca(new Glasac("Sven", "Milinkovic-Savic", new Adresa("Rim", "ulica1", 71000, "123"), new DateTime(2000, 5, 21), "131T121", "2105000222222"));
-            r.dodajGlasaca(new Glasac("Edin", "Dzeko", new Adresa("Sarajevo", "ulica2", 71020, "231"), new DateTime(2000, 12, 31), "131T231", "3112000333333"));
-            r.dodajGlasaca(new Glasac("Robert", "Prosinecki", new Adresa("Zagreb", "ulica3", 71120, "132"), new DateTime(2000, 10, 10), "131T212", "1010000444444"));
-            r.dodajGlasaca(new Glasac("Branislav", "Ivanovic", new Adresa("Novi Sad", "ulica4", 71000, "5"), new DateTime(1999, 2, 15), "131T415", "1502999555555"));
-            g1 = new Glasac("Marcelo", "Brozovic", new Adresa("Zadar", "Olivera Dragojevica", 79182, "333"), new DateTime(1992, 1, 15), "113T214", "1501992666666");
-            g2 = new Glasac("Dominik", "Livakovic", new Adresa("Gol", "Stativa", 71234, "1"), new DateTime(1995, 12, 11), "121T617", "1112995777777");
-            k1 = new Kandidat("Marija", "Kiri", new Adresa("Poljska", "Warsava", 71224, "321"), new DateTime(1989, 10, 10), "112T657", "1010989888888");
-            k1.pridruziStranci(s1);
-            k2 = new Kandidat("Marko", "Livaja", new Adresa("Split", "Poljud", 74928, "10"), new DateTime(1992, 10, 10), "113T157", "1010992999999");
-            r.dodajStranku(s1);
-            r.dodajStranku(s2);
-            r.dodajGlas();
-            r.dodajGlas();
-            r.dodajKandidata(k2);
-            r.dodajKandidata(k1);
+            registar.dodajGlasaca(new Glasac("Muhamed", "Masnopita", new Adresa("Iljas", "Ljesevo", 71380, "252"), new DateTime(1992, 11, 15), "165T214", "1211992111111"));
+            registar.dodajGlasaca(new Glasac("Sven", "Milinkovic-Savic", new Adresa("Rim", "ulica1", 71000, "123"), new DateTime(2000, 5, 21), "131T121", "2105000222222"));
+            registar.dodajGlasaca(new Glasac("Edin", "Dzeko", new Adresa("Sarajevo", "ulica2", 71020, "231"), new DateTime(2000, 12, 31), "131T231", "3112000333333"));
+            registar.dodajGlasaca(new Glasac("Robert", "Prosinecki", new Adresa("Zagreb", "ulica3", 71120, "132"), new DateTime(2000, 10, 10), "131T212", "1010000444444"));
+            registar.dodajGlasaca(new Glasac("Branislav", "Ivanovic", new Adresa("Novi Sad", "ulica4", 71000, "5"), new DateTime(1999, 2, 15), "131T415", "1502999555555"));
+            glasac1 = new Glasac("Marcelo", "Brozovic", new Adresa("Zadar", "Olivera Dragojevica", 79182, "333"), new DateTime(1992, 1, 15), "113T214", "1501992666666");
+            glasac2 = new Glasac("Dominik", "Livakovic", new Adresa("Gol", "Stativa", 71234, "1"), new DateTime(1995, 12, 11), "121T617", "1112995777777");
+            kandidat1 = new Kandidat("Marija", "Kiri", new Adresa("Poljska", "Warsava", 71224, "321"), new DateTime(1989, 10, 10), "112T657", "1010989888888");
+            kandidat1.pridruziStranci(s1);
+            kandidat2 = new Kandidat("Marko", "Livaja", new Adresa("Split", "Poljud", 74928, "10"), new DateTime(1992, 10, 10), "113T157", "1010992999999");
+            registar.dodajStranku(s1);
+            registar.dodajStranku(s2);
+            registar.dodajGlas();
+            registar.dodajGlas();
+            registar.dodajKandidata(kandidat2);
+            registar.dodajKandidata(kandidat1);
             List<Kandidat> l1 = new List<Kandidat>();
-            l1.Add(k1);
-            g1.glasaj(l1);
-            g1.glasaj(s1);
-        }
-
-        [TestInitialize()]
-        public void InicijalizacijaPrijeSvakogTesta()
-        {
-            
-           
-           
-            
+            l1.Add(kandidat1);
+            glasac1.glasaj(l1);
+            glasac1.glasaj(s1);
+            kandidat1.dodajGlas(glasac1);
+            kandidat1.dodajGlas(glasac2);
+            kandidat1.dodajGlas(new Glasac("Mensur", "Mujdza", new Adresa("Sarajevo", "Hasima Spahica", 71000, "bb"), new DateTime(1999, 1, 23), "23T657", "2301999170035"));
+            registar.dodajStranku(s1);
+            kandidat2.pridruziStranci(s1);
+            s1.dodajGlas(glasac1);
+            s1.dodajGlas(glasac2);
+            kandidat2.dodajGlas(glasac1);
+            kandidat2.dodajGlas(glasac2);
+            registar.dodajGlas();
+            registar.dodajGlas();
+            s1.dodajGlas(new Glasac("kk", "jjiajas", new Adresa("iqiqi", "jksja", 18293, "bb"), new DateTime(1999, 1, 21), "211T61", "210199921237"));
+            s1.dodajGlas(new Glasac("ki", "jjiajas", new Adresa("iqiqi", "jksja", 18293, "bb"), new DateTime(1999, 1, 21), "221T98", "210199921290"));
+            s1.dodajGlas(new Glasac("km", "jjijajas", new Adresa("iqiqi", "jksja", 18293, "bb"), new DateTime(1999, 1, 21), "281T63", "210199921276"));
+            s1.dodajGlas(new Glasac("kl", "jjlijas", new Adresa("iqiqi", "jksja", 18293, "bb"), new DateTime(1999, 1, 21), "241T81", "210199921256"));
+            s1.dodajGlas(new Glasac("ko", "jjilijas", new Adresa("iqiqi", "jksja", 18293, "bb"), new DateTime(1999, 1, 21), "281T62", "210199921209"));
         }
 
         [TestMethod]
         public void TestMetodeGetBrojGlasaca()
         {
-            Assert.AreEqual(2, r.getBrojGlasaca());
+            Assert.AreEqual(4, registar.getBrojGlasaca());
         }
 
         [TestMethod]
         public void TestMetodeGetGlasaci()
         {
-            r.dodajGlasaca(g1);
-            Assert.IsTrue(r.getGlasaci().Contains(g1));
-            Assert.IsFalse(r.getGlasaci().Contains(g2));
+            registar.dodajGlasaca(glasac1);
+            Assert.IsTrue(registar.getGlasaci().Contains(glasac1));
+            Assert.IsFalse(registar.getGlasaci().Contains(glasac2));
         }
 
         [TestMethod]
         public void TestMetodeGetNezavisneKandidate()
         {
-            Assert.AreEqual(1, r.getNezavisnikandidati().Count);
+            Assert.AreEqual(1, registar.getNezavisnikandidati().Count);
         }
 
 
         [TestMethod]
         public void TestMetodeGetStranke()
         {
-            Assert.AreEqual(2, r.getStranke().Count);
-            Assert.IsTrue(r.getStranke().Contains(s2));
-            Assert.IsTrue(r.getStranke().Contains(s1));
-            r.dodajStranku(s2);
-            Assert.IsTrue(r.getStranke().Contains(s2));
+            Assert.AreEqual(3, registar.getStranke().Count);
+            Assert.IsTrue(registar.getStranke().Contains(s2));
+            Assert.IsTrue(registar.getStranke().Contains(s1));
+            registar.dodajStranku(s2);
+            Assert.IsTrue(registar.getStranke().Contains(s2));
         }
 
         [TestMethod]
         public void TestMetodeIspisRezultata2()
         {
-            r.dodajStranku(s1);
-            k2.pridruziStranci(s1);
-            s1.dodajGlas(g1);
-            s1.dodajGlas(g2);
-            k2.dodajGlas(g1);
-            k2.dodajGlas(g2);
-            r.dodajGlas();
-            r.dodajGlas();
-            Assert.AreEqual("Socijaldemokratska partija:\nMarija Kiri broj glasova: 1 procenat osvojenih glasova: 33.33333333333333%\nMarko Livaja broj glasova: 2 procenat osvojenih glasova: 66.66666666666666%\n\n", r.ispisRezultata());
+            
+            Assert.AreEqual("Socijaldemokratska partija:\n\n", registar.ispisRezultata());
         }
 
         [TestMethod]
         public void TestMetodeDajIzlaznost()
         {
-            Assert.AreEqual(0.4, r.dajIzlaznost());
+            Assert.AreEqual(0.8, registar.dajIzlaznost());
         }
 
         [TestMethod]
         public void TestMetodeDajStrankeKojeSuPresleCenzus()
         {
-            Assert.IsTrue(r.dajStrankeKojeSuPresleCenzus().Contains(s1));
-            Assert.IsFalse(r.dajStrankeKojeSuPresleCenzus().Contains(s2));
+            Assert.IsTrue(registar.dajStrankeKojeSuPresleCenzus().Contains(s1));
+            Assert.IsFalse(registar.dajStrankeKojeSuPresleCenzus().Contains(s2));
         }
 
         [TestMethod]
         public void TestMetodeDajKandidateKojiSuOsvojiliMandat()
         {
-            Assert.IsTrue(r.dajKandidateKojiSuOsvojiliMandat().Contains(k1));
-            Assert.IsFalse(r.dajKandidateKojiSuOsvojiliMandat().Contains(k2));
+            Assert.IsFalse(registar.dajKandidateKojiSuOsvojiliMandat().Contains(kandidat1));
+            Assert.IsTrue(registar.dajKandidateKojiSuOsvojiliMandat().Contains(kandidat2));
         }
 
         /*static IEnumerable<object[]> Registar1
