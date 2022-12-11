af94779142c6cfc300257493fc56d9512bca473a
Esma Zejnilovic <esma.zejnilovic001@gmail.com>
2022-12-11 12:50:21 +0100

    fix

--- VVSZadaceTest/RegistarTest.cs
+++ VVSZadaceTest/RegistarTest.cs
@@ -1,9 +1,11 @@
-﻿using VVSZadaca1;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VVSZadace;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace VVSZadaceTests
 {
