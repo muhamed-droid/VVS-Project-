55421003c458c31154d11294bf31a8d3a52a6fa3
muhamed-droid <mmasnopita1@etf.unsa.ba>
2023-01-07 15:35:19 +0100

    greska ispravljenja

--- VVSZadace/Administrator.cs
+++ VVSZadace/Administrator.cs
@@ -24,7 +24,7 @@ namespace VVSZadace
         {
             brojac--;
             String tajnaSifra = "VVS20222023";
-            if (!sifra.ToUpper().Equals(tajnaSifra)
+            if (!sifra.ToUpper().Equals(tajnaSifra))
             {
                 if (brojac == 0)
                 {
